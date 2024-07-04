19cccbb5359ad6a82f37a905ecf06d8f30e764d4
kedi <omerinyerix@gmail.com>
2024-07-04 22:27:36 +0300

    degiskenler
    degiskenler notlar ve ornekler

---
+++ konsolapp 2/konsolapp 2/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace konsolapp_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region string to int
+            string b = "123";
+            int a = Convert.ToInt32(b);
+            //Console.WriteLine(a);            
+            //Console.Read();
+
+            #endregion
+
+            #region double to string
+            double qu = 12.3;
+            string ku = Convert.ToString(qu);
+            //Console.WriteLine(ku);
+            //Console.Read();
+            #endregion
+
+            #region bool to string
+            bool ax = true;
+            string xa = Convert.ToString(ax);
+            //Console.WriteLine(xa);
+            //Console.ReadLine();
+            #endregion
+
+            #region string to boolean
+            string aw = "true";
+            Boolean owo = Convert.ToBoolean(aw);
+            //Console.WriteLine(owo);
+            //Console.ReadLine();
+            #endregion
+
+            #region int.parse
+            //parse metodu sadece string ifadeeleri cevirir
+            string koa = "123";
+            int kuj = int.Parse(koa);
+            //Console.WriteLine(kuj);
+            //Console.ReadLine();
+            #endregion
+
+            #region string to decimal
+            string hok = "321";
+            decimal uk = int.Parse(hok);
+            //Console.WriteLine(uk);
+            //Console.ReadLine();
+            #endregion
+
+            #region char to int 
+            char luk = 'o';
+            int jul = Convert.ToInt32(luk);
+            //Console.WriteLine(jul);
+            //Console.ReadLine();
+
+            int j = 110;
+            char hu = Convert.ToChar(j);
+            //Console.WriteLine(hu);
+            //Console.ReadLine();
+            #endregion
+
+            #region checked - uncheckhed
+            checked
+            {
+                int oky = 122;
+                byte ru = Convert.ToByte(oky);
+                Console.WriteLine(ru);
+                Console.ReadLine();
+            }
+            
+            
+            #endregion
+
+
+        }
+    }
+}
---
+++ konsolapp/konsolapp/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace konsolapp
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            #region degiskenler
+            //string ad;
+            //char basHarf;
+            //int sayi;
+            //bool evetHayir;
+            #endregion
+
+
+            #region kullanim 1
+            string ad;
+            ad = "omer";
+            #endregion
+
+            #region kullanim 2
+            string soyad = "altuntas";
+            #endregion
+
+            #region kullanim 3
+            string a, b, c;
+            a = "a";
+            b = "b";
+            c = "c";
+            #endregion
+
+            #region degiskeni degiskene atama
+            int x = 2;
+            int y = x;
+            y = y - 2;
+            int z = x - y;
+            #endregion
+
+            #region var ve object
+            var q = "q";
+            var s = 123; //var dinamic bir degiskendir karsisina ne yazarsan onun turune donusur
+
+            object k = "sabfasdf";
+            object l = 123; //objectise icine ekledigin sey ne olursa olsun onu objecte cevirir yani boxing yapar 
+            #endregion
+
+            #region boxing
+            object kuk = 12;
+            #endregion
+            #region unboxing
+            string hoh = (string)kuk;
+            #endregion
+
+
+            #region tip cevrimleri
+            string ucak = "123";
+            int kapi = Convert.ToInt32(ucak);
+
+            Console.WriteLine(ucak);
+            Console.ReadLine();
+            #endregion
+
+
+        }
+    }
+}
